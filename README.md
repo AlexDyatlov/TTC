3acb24960e2dba97af01dd1c05cc4f79f460a59f
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-12-22 15:21:46 +0300

    added layout for PC

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TTC
 
 Тестовое задание - вёрстка нескольких блоков.
+
+<p align="center">
+  <img src="https://github.com/AlexDyatlov/TTC/raw/master/build/images/readme.png">
+</p>
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
 
+  $('.social__dropdown').on('click', function(){
+    $(this).next().slideToggle();
+    $(this).toggleClass('social__dropdown--active');
+  });
+
 });
